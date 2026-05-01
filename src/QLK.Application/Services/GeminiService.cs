9e6425e0bb28a2332f71adadd2bdb4fa1965c33a
VDP <voduyphuong26042004@gmail.com>
2026-05-01 10:23:43 +0700

    fix: restore essential namespaces for GeminiService build

--- src/QLK.Application/Services/GeminiService.cs
+++ src/QLK.Application/Services/GeminiService.cs
@@ -2,6 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using QLK.Application.DTOs.AI;
 using QLK.Infrastructure.Data;
 using QLK.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 
 namespace QLK.Application.Services
